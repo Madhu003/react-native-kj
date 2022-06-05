2e096dbbef514650ea68894a2a6f075d79368a6a
Madhu003 <madhusudanarya003@gmail.com>
2022-06-05 19:31:18 +0530

    number format added

--- screens/WhatsApp.js
+++ screens/WhatsApp.js
@@ -1,15 +1,29 @@
-import * as React from 'react';
-import { StyleSheet, Button, View, TextInput, Linking } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Button, View, TextInput, Linking } from "react-native";
 
 function WhatsApp({ navigation }) {
   const [number, onChangeNumber] = React.useState(null);
 
+  React.useEffect(() => {
+    console.log(number);
+  }, [number]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TextInput style={styles.input} value={number} onChangeText={onChangeNumber} />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <TextInput
+        style={styles.input}
+        value={number}
+        keyboardType="number-pad"
+        onChangeText={onChangeNumber}
+        placeholder="Mobile Number"
+      />
       <Button
-        onPress={() => Linking.openURL(`whatsapp://send?text=hello&phone=+91${number}`)}
-        title="Go" />
+        onPress={() =>
+          Linking.openURL(`whatsapp://send?text=hello&phone=+91${number}`)
+        }
+        disabled={(number + "").length != 10}
+        title="Go"
+      />
     </View>
   );
 }
